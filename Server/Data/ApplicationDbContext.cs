b8b9befbbae158566beb5d1d4fc0b01f176cedc2
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-22 19:38:51 +0500

    FEAT: Add `Book` & `Category` models, setup `ApplicationDbContext` and create migration

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BlazorBookHub.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorBookHub.Server.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Category> Categories => Set<Category>();
+    }
+}

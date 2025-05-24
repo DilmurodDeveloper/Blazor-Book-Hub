6cc0efd718eec7e43a72f73da5d33cf5964d1bad
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-24 11:29:18 +0500

    STYLE: Add `Login` and `Register` UI with functional validation and gradient background

---
+++ Client/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿window.fadeIn = (id) => {
+    const el = document.getElementById(id);
+    if (el) {
+        el.style.opacity = 0;
+        el.style.transition = "opacity 1s ease-in-out";
+        setTimeout(() => {
+            el.style.opacity = 1;
+        }, 100);
+    }
+};
